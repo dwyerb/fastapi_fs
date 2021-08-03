2d187a363cbd6df218b21524b9195cb4c7dee303
dwyerb <brendandwyer@hotmail.com>
2021-08-03 15:56:09 -0400

    Update with type param in path

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id):
+async def read_item(item_id: int):
     return{"Item ID:":item_id}
