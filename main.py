7c403b514994911236441b84213f4aae7ad912bc
dwyerb <brendandwyer@hotmail.com>
2021-08-03 16:12:07 -0400

    Fixes #1

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    saxnet = "saxnet"
+    marshall = "marshall"
+    fender = "fender"
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/models/{model_name}")
+async def getModel(model_name:ModelName):
+    if(model_name == ModelName.alexnet):
+        return{"Model Name:":"Deep Learning"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
