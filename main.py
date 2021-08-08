e0d5ea82f9c626b3a32a9bfde57752e588171c4e
dwyerb <brendandwyer@hotmail.com>
2021-08-08 17:56:44 -0400

    Completes issue #2

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ async def root():
 async def getModel(model_name:ModelName):
     if(model_name == ModelName.alexnet):
         return{"Model Name:":"Deep Learning"}
+    if(model_name.value == 'saxnet'):
+        return{"Model Name": model_name,"message":"LeCNN Something Something"}
+    if (model_name == ModelName.marshall):
+        return {"Model Name": model_name, "message": "We Are Marshall"}
+    else:
+        return {"Model Name": model_name, "message": "Sherd"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
